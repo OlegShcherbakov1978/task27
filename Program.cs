4a86718ddcb6fca8c4d9091e728ef3b5ff914aea
Oleg <olmag@inbox.ru>
2023-01-27 17:31:14 +0300

    task27

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 27: Напишите программу, которая принимает
+// на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+int InputNumber(string str)
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+
+        System.Console.WriteLine("введено некорректное число");
+    }
+    return number;
+
+}
+
+int NumSum(int num)
+{
+    int result = 0;
+    int[] digits = Array.ConvertAll(num.ToString("0").ToCharArray(), ch => ch - '0');
+    for (int i = 0; i < digits.Length; i++)
+        result = result + digits[i];
+    return result;
+}
+
+int N = InputNumber("введитее число: ");
+
+Console.WriteLine(NumSum(N));
+
